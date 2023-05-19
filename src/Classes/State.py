40299a91d3386ba9ffd65c87cc2989b9ee51f7b5
Sean McOwen <seanmcowen@gmail.com>
2023-05-19 11:44:26 -0400

    Add in classes for state and variables

--- src/Classes/State.py
+++ src/Classes/State.py
@@ -4,10 +4,34 @@ from typing import Dict
 class State:
 
     def __init__(self, data: Dict):
-        pass
+        self.name = data["name"]
+        self.notes = data["notes"]
+        self.variables = data["variables"]
+        self._write_variable_map()
+
+    def _write_variable_map(self) -> None:
+        """
+        Convenience function which maps variable names to the objects
+        """
+
+        self.variable_map = {}
+
+        # Iterate through
+        for var in self.variables:
+            key = var.name
+
+            # Check variable name not repeated
+            assert var.name not in self.variable_map, "Variable name {} is already present in variables!".format(
+                key)
+
+            self.variable_map[key] = var
 
 
 class StateVariable:
 
     def __init__(self, data: Dict):
-        pass
+        self.type = data["type"]
+        self.name = data["name"]
+        self.description = data["description"]
+        self.symbol = data["symbol"]
+        self.domain = data["domain"]
