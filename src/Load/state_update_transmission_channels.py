69b8a583c89da3d13e91f7517c522c5575d764da
Sean McOwen <seanmcowen@gmail.com>
2023-06-18 12:28:35 -0400

    Add checks and mapping for the state update transmission

--- src/Load/state_update_transmission_channels.py
+++ src/Load/state_update_transmission_channels.py
@@ -3,11 +3,12 @@ from ..Classes import StateUpdateTransmissionChannel
 from .general import check_json_keys
 
 
-def convert_state_update_transmission_channel(data: Dict) -> StateUpdateTransmissionChannel:
+def convert_state_update_transmission_channel(data: Dict, ms: Dict) -> StateUpdateTransmissionChannel:
     """Function to convert dictionary to state update transmission channel
 
     Args:
         data (Dict): The data to convert
+        ms (Dict): MathSpec dictionary
 
     Returns:
         StateUpdateTransmissionChannel: Action transmission channel object
@@ -15,6 +16,23 @@ def convert_state_update_transmission_channel(data: Dict) -> StateUpdateTransmis
 
     # Check the keys are correct
     check_json_keys(data, "State Update Transmission Channel")
+
+    # Assert the origin is in mechanisms then map
+    origin = data["origin"]
+    assert origin in ms["Mechanisms"]
+    data["origin"] = ms["Mechanisms"][origin]
+
+    # Assert entity is in entities then map
+    entity = data["entity"]
+    assert entity in ms["Entities"]
+    data["entity"] = ms["Entities"][entity]
+
+    # Assert variable is in entity state then map
+    entity = data["entity"]
+    variable = data["variable"]
+
+    assert variable in entity.state.variable_map
+    data["variable"] = entity.state.variable_map[variable]
 
     # Copy
     data = data.copy()
@@ -34,4 +52,4 @@ def load_state_update_transmission_channels(ms: Dict, json: Dict) -> None:
     ms["State Update Transmission Channels"] = []
     for sutc in json["State Update Transmission Channels"]:
         ms["State Update Transmission Channels"].append(
-            convert_state_update_transmission_channel(sutc))
+            convert_state_update_transmission_channel(sutc, ms))
