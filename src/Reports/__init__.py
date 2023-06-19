5376e490febc8575113276a6e6446fcec02a1901
Sean McOwen <seanmcowen@gmail.com>
2023-06-18 22:55:27 -0400

    Mechanism reports added

--- src/Reports/__init__.py
+++ src/Reports/__init__.py
@@ -1,3 +1,4 @@
 from .node_map import create_action_chains_graph
 from .boundary_actions import write_out_boundary_actions
 from .policies import write_out_policies
+from .mechanisms import write_out_mechanisms
---
+++ src/Reports/mechanisms.py
@@ -0,0 +1,50 @@
+from ..Classes import Mechanism, MathSpec
+from typing import List
+
+
+def write_out_mechanism(mechanism: Mechanism) -> str:
+    out = ""
+    out += "<h3>"
+    out += mechanism.label
+    out += "</h3>"
+    out += "<p>"
+    out += mechanism.description
+    out += "</p>"
+
+    out += "<h4>Preceded By:</h4>\n"
+    for i, x in enumerate(mechanism.called_by):
+        out += "<p>"
+        out += "{}. {}".format(i+1, x.name)
+        out += "</p>"
+
+    out += "<h4>Domain Spaces:</h4>\n"
+    for i, x in enumerate(mechanism.domain):
+        out += "<p>"
+        out += "{}. {}".format(i+1, x.__name__)
+        out += "</p>"
+
+    out += "<h4>State Updates:</h4>\n"
+    for i, x in enumerate(mechanism.updates):
+        out += "<p>"
+        out += "{}. {}.{}".format(i+1, x[0].name, x[1].name)
+        out += "</p>"
+
+    out += "<h4>Constraints:</h4>\n"
+    for i, x in enumerate(mechanism.constraints):
+        out += "<p>"
+        out += "{}. {}\n".format(i+1, x)
+        out += "</p>"
+
+    out += "<h4>Logic:</h4>\n"
+    out += "<p>"
+    out += mechanism.logic
+    out += "</p>"
+    return out
+
+
+def write_out_mechanisms(ms: MathSpec, mechanism_names: List[str]) -> str:
+    out = "<h2>Mechanisms</h2>"
+    for name in mechanism_names:
+        out += write_out_mechanism(ms.mechanisms[name])
+
+    return out
