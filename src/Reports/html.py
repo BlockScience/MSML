4f0bb29a99d85b94802c2528dbd57f3d1c436d2e
Sean McOwen <seanmcowen@gmail.com>
2023-06-19 18:29:42 -0400

    Graph creation for actions

--- src/Reports/html.py
+++ src/Reports/html.py
@@ -2,11 +2,19 @@ from ..Classes import MathSpec
 from .boundary_actions import write_out_boundary_actions
 from .policies import write_out_policies
 from .mechanisms import write_out_mechanisms
+from .general import load_svg_graphviz
+from .node_map import create_action_chains_graph
 
 
 def write_basic_report_full(ms: MathSpec, directory: str, name: str) -> None:
     out = ""
-    out += write_out_boundary_actions(ms, list(ms.boundary_actions.keys()))
+    out += "<h2>Action Maps</h2>"
+    behaviors = list(ms.boundary_actions.keys())
+    for behavior in behaviors:
+        out += load_svg_graphviz(create_action_chains_graph(ms,
+                                                            [behavior], behavior))
+
+    out += write_out_boundary_actions(ms, behaviors)
     out += write_out_policies(ms, list(ms.policies.keys()))
     out += write_out_mechanisms(ms, list(ms.mechanisms.keys()))
 
