3a4aee8107c09905b9a7190246ff952201d7c5ca
Sean McOwen <seanmcowen@gmail.com>
2023-08-16 10:28:48 -0400

    Connect

--- examples/Starter Repo/src/Policies/Dummy.py
+++ examples/Starter Repo/src/Policies/Dummy.py
@@ -1,6 +1,11 @@
-from ..Spaces import create_node_space
+from ..Spaces import dummy_space1, dummy_space2
 
 dummy_policy_option = {}
 
-dummy_policy = {}
-
+dummy_policy = {"name": "Dummy Policy",
+                        "description": "Dummy Policy",
+                        "constraints": [],
+                        "policy_options": [],
+                        "domain": [dummy_space1],
+                        "codomain": [dummy_space2],
+                        "parameters_used": []}
