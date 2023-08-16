d9beb3425665ea8f0a660b854ade45e83457a5db
Sean McOwen <seanmcowen@gmail.com>
2023-08-16 10:54:44 -0400

    Finish the baseline of the starter repo

---
+++ examples/Starter Repo/src/StateUpdateTransmissionChannels/Dummy.py
@@ -0,0 +1,6 @@
+dummy_transmission_channels = []
+
+dummy_transmission_channels.append({"origin": "Dummy Mechanism",
+                                     "entity": "Dummy",
+                                     "variable": "Variable A",
+                                     "optional": False})
--- examples/Starter Repo/src/StatefulMetrics/__init__.py
+++ examples/Starter Repo/src/StatefulMetrics/__init__.py
@@ -1,1 +1,3 @@
-stateful_metrics = {}
+from .Dummy import dummy_stateful_metric
+
+stateful_metrics = {"Dummy Stateful Metrics": dummy_stateful_metric}
