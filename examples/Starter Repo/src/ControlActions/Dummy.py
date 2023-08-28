6a035f04bed2a468c159239ee46ecb4396802ccf
Sean McOwen <seanmcowen@gmail.com>
2023-08-28 15:16:11 -0400

    Create dummy repo control action

---
+++ examples/Starter Repo/src/ControlActions/Dummy.py
@@ -0,0 +1,10 @@
+from ..Spaces import dummy_space2
+
+dummy_control_action = {
+    "name": "Dummy Control Action",
+    "description": "Dummy",
+    "constraints": [],
+    "boundary_action_options": [],
+    "codomain": [dummy_space2],
+    "parameters_used": [],
+}
