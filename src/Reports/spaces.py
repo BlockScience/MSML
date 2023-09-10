fb702433b447052936efe0c9500747ac42766b90
Sean McOwen <seanmcowen@gmail.com>
2023-09-10 12:08:19 -0400

    Finish spaces

--- src/Reports/spaces.py
+++ src/Reports/spaces.py
@@ -3,45 +3,20 @@ from typing import List, TypedDict
 
 
 def write_out_space(space: TypedDict) -> str:
-    """out = ""
+    out = ""
     out += "<h3>"
-    out += mechanism.label
+    out += space.__name__
     out += "</h3>"
+
+    d = space.__annotations__
+    d = ",<br/>".join(["{}: {}".format(a, b.__name__) for a,b in zip(d.keys(), d.values())])
+    d = "{" + d + "}"
+
     out += "<p>"
-    out += mechanism.description
+    out += d
     out += "</p>"
 
-    out += "<h4>Preceded By:</h4>\n"
-    for i, x in enumerate(mechanism.called_by):
-        x = x[0]
-        out += "<p>"
-        out += "{}. {}".format(i+1, x.name)
-        out += "</p>"
-
-    out += "<h4>Domain Spaces:</h4>\n"
-    for i, x in enumerate(mechanism.domain):
-        out += "<p>"
-        out += "{}. {}".format(i+1, x.__name__)
-        out += "</p>"
-
-    out += "<h4>State Updates:</h4>\n"
-    for i, x in enumerate(mechanism.updates):
-        out += "<p>"
-        out += "{}. {}.{}".format(i+1, x[0].name, x[1].name)
-        out += "</p>"
-
-    out += "<h4>Constraints:</h4>\n"
-    for i, x in enumerate(mechanism.constraints):
-        out += "<p>"
-        out += "{}. {}\n".format(i+1, x)
-        out += "</p>"
-
-    out += "<h4>Logic:</h4>\n"
-    out += "<p>"
-    out += mechanism.logic
-    out += "</p>"
-    return out"""
-    return "<p>HOLD</p>"
+    return out
 
 
 def write_out_spaces(ms: MathSpec, spaces: List[str]) -> str:
