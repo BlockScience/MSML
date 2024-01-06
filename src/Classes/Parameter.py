a12ec5b4aca2ebc67c955e0f8e566978dae57821
SeanMcOwen <seanmcowen@gmail.com>
2024-01-06 13:38:19 -0500

    Finish param classes

--- src/Classes/Parameter.py
+++ src/Classes/Parameter.py
@@ -4,29 +4,34 @@ from typing import Dict
 
 
 class ParameterContainer:
-
     def __init__(self, data: Dict):
         self.data = data
 
         # TODO: Get rid of this when params are fully built out
         self.all_parameters = []
         for x in self.data.values():
-            x = x["parameters"]
-            x = [y["name"] for y in x]
+            x = x.parameters
+            x = [y.name for y in x]
             self.all_parameters.extend(x)
+
 
 # A set of parameters
 
 
 class ParameterSet:
-
     def __init__(self, data: Dict):
-        pass
+        self.name = data["name"]
+        self.notes = data["notes"]
+        self.parameters = data["parameters"]
+
 
 # Individual Parameter
 
 
 class Parameter:
-
     def __init__(self, data: Dict):
-        pass
+        self.variable_type = data["variable_type"]
+        self.name = data["name"]
+        self.description = data["description"]
+        self.symbol = data["symbol"]
+        self.domain = data["domain"]
