571a7fed6d9c2866605d8ee6141799fa84ac8b5e
SeanMcOwen <seanmcowen@gmail.com>
2024-01-06 13:41:59 -0500

    Check no repeated parameter names

--- src/Classes/Parameter.py
+++ src/Classes/Parameter.py
@@ -7,12 +7,14 @@ class ParameterContainer:
     def __init__(self, data: Dict):
         self.data = data
 
-        # TODO: Get rid of this when params are fully built out
         self.all_parameters = []
         for x in self.data.values():
             x = x.parameters
             x = [y.name for y in x]
             self.all_parameters.extend(x)
+        assert len(set(self.all_parameters)) == len(
+            self.all_parameters
+        ), "A parameter name is repeated!"
 
 
 # A set of parameters
