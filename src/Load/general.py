cae481f0d9c386b490841ef657543cfa2cd7d5a4
SeanMcOwen <seanmcowen@gmail.com>
2024-01-06 12:40:40 -0500

    Add param checking

--- src/Load/general.py
+++ src/Load/general.py
@@ -89,6 +89,7 @@ def check_json_keys(json: Dict, check_set_key: str) -> None:
         ],
         "Boundary Action Option": ["name", "description", "logic"],
         "Parameter Set": ["name", "notes", "parameters"],
+        "Parameter": ["variable_type", "name", "description", "symbol", "domain"],
     }
 
     check_set = check_sets[check_set_key]
--- src/Load/parameters.py
+++ src/Load/parameters.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from ..Classes import ParameterContainer, ParameterSet
+from ..Classes import ParameterContainer, ParameterSet, Parameter
 from .general import check_json_keys
 
 
@@ -7,7 +7,19 @@ def convert_parameter_set(data: Dict) -> ParameterSet:
     # Check the keys are correct
     check_json_keys(data, "Parameter Set")
 
-    return data
+    # Copy
+    data = data.copy()
+
+    # Convert the parameters
+    new_parameters = []
+    for param in data["parameters"]:
+        print(param)
+        check_json_keys(param, "Parameter")
+        new_parameters.append(Parameter(param))
+    data["parameters"] = new_parameters
+
+    # Build the state object
+    return ParameterSet(data)
 
 
 def load_parameters(ms: Dict, json: Dict) -> None:
