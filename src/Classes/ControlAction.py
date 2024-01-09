289468b61523b3bcff90efe9a656f46b406a1d68
SeanMcOwen <seanmcowen@gmail.com>
2024-01-09 09:49:42 -0500

    Add block base class

--- src/Classes/ControlAction.py
+++ src/Classes/ControlAction.py
@@ -1,19 +1,12 @@
 from typing import Dict
+from .Block import Block
 
 
-class ControlAction:
+class ControlAction(Block):
     def __init__(self, data: Dict):
-        self.name = data["name"]
-        self.description = data["description"]
-        self.constraints = data["constraints"]
+        data["domain"] = None
+        super().__init__(data)
         self.control_action_options = data["control_action_options"]
-        self.codomain = data["codomain"]
-        self.parameters_used = data["parameters_used"]
-        if "label" in data:
-            self.label = data["label"]
-        else:
-            self.label = self.name
-        self.calls = []
 
 
 class ControlActionOption:
