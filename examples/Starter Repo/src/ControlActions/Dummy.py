118f4c69633bd406abca6dce1e28af260f4f9590
SeanMcOwen <seanmcowen@gmail.com>
2024-01-08 23:03:45 -0500

    Add control action option

--- examples/Starter Repo/src/ControlActions/Dummy.py
+++ examples/Starter Repo/src/ControlActions/Dummy.py
@@ -1,10 +1,16 @@
 from ..Spaces import dummy_space2
 
+dummy_control_action_v1_option = {
+    "name": "V1 Dummy Control",
+    "description": "Description",
+    "logic": "A+B=C",
+}
+
 dummy_control_action = {
     "name": "Dummy Control Action",
     "description": "Dummy",
     "constraints": [],
-    "control_action_options": [],
+    "control_action_options": [dummy_control_action_v1_option],
     "codomain": [dummy_space2],
     "parameters_used": [],
 }
