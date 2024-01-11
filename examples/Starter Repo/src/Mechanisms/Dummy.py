7b6076b85779f02a4460e3ca476aa737ab86e149
SeanMcOwen <seanmcowen@gmail.com>
2024-01-11 17:08:44 -0500

    Update domain and codomain in example

--- examples/Starter Repo/src/Mechanisms/Dummy.py
+++ examples/Starter Repo/src/Mechanisms/Dummy.py
@@ -1,8 +1,10 @@
 from ..Spaces import dummy_space2
 
-dummy_mechanism = {"name": "Dummy Mechanism",
-                           "description": "Dummy Mechanism",
-                           "constraints": ["Constaint 1"],
-                           "logic": "Logic for update",
-                           "domain": [dummy_space2],
-                           "parameters_used": []}
+dummy_mechanism = {
+    "name": "Dummy Mechanism",
+    "description": "Dummy Mechanism",
+    "constraints": ["Constaint 1"],
+    "logic": "Logic for update",
+    "domain": (dummy_space2,),
+    "parameters_used": [],
+}
