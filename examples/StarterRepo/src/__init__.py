9206a45a350e8072636c8ebf3268e76005218cad
SeanMcOwen <seanmcowen@gmail.com>
2024-01-12 23:15:14 -0500

    Working through wiring

--- examples/StarterRepo/src/__init__.py
+++ examples/StarterRepo/src/__init__.py
@@ -9,15 +9,19 @@ from .Entities import entities
 from .BoundaryActions import boundary_actions
 from .ActionTransmissionChannels import action_transmission_channels
 from .ControlActions import control_actions
+from .Wiring import wiring
 
-math_spec_json = {"Policies": policies,
-                  "Spaces": spaces,
-                  "State": state,
-                  "Stateful Metrics": stateful_metrics,
-                  "State Update Transmission Channels": state_update_transmission_channels,
-                  "Parameters": parameters,
-                  "Mechanisms": mechanism,
-                  "Entities": entities,
-                  "Boundary Actions": boundary_actions,
-                  "Action Transmission Channels": action_transmission_channels,
-                  "Control Actions": control_actions}
+math_spec_json = {
+    "Policies": policies,
+    "Spaces": spaces,
+    "State": state,
+    "Stateful Metrics": stateful_metrics,
+    "State Update Transmission Channels": state_update_transmission_channels,
+    "Parameters": parameters,
+    "Mechanisms": mechanism,
+    "Entities": entities,
+    "Boundary Actions": boundary_actions,
+    "Action Transmission Channels": action_transmission_channels,
+    "Control Actions": control_actions,
+    "Wiring": wiring,
+}
