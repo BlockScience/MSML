e8d1063efe633bb533279d75fca7fa3fd940ec37
SeanMcOwen <seanmcowen@gmail.com>
2024-01-27 00:27:15 -0500

    Add global state

--- examples/StarterRepo/src/State/__init__.py
+++ examples/StarterRepo/src/State/__init__.py
@@ -1,3 +1,4 @@
 from .Dummy import dummy_state
+from .Global import global_state
 
-state = {"Dummy": dummy_state,}
+state = {"Dummy": dummy_state, "Global": global_state}
--- src/Load/states.py
+++ src/Load/states.py
@@ -39,5 +39,6 @@ def load_states(ms: Dict, json: Dict) -> None:
     """
 
     ms["State"] = {}
+    assert "Global" in json["State"], "Global has to be in the state"
     for key in json["State"]:
         ms["State"][key] = convert_state(json["State"][key])
