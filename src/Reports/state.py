3753819c7f1a18f55ee7d0a6bbedf36d4a3c3b9c
SeanMcOwen <seanmcowen@gmail.com>
2024-02-07 13:21:31 -0500

    Markdown table

--- src/Reports/state.py
+++ src/Reports/state.py
@@ -33,6 +33,33 @@ def write_state_variable_table(target_state, links=False):
     return table
 
 
+def write_state_variable_table_markdown(target_state, links=False):
+    table = """| Name | Description | Type | Symbol | Domain |
+| --- | --- | --- | --- | --- |
+"""
+
+    for var in target_state.variables:
+        table_vars = [
+            var.name,
+            var.description,
+            var.type.__name__,
+            var.symbol,
+            var.domain,
+        ]
+        table += "|"
+        for i, tv in enumerate(table_vars):
+            if tv:
+                if links and i == 2:
+                    table += "[[{}]]".format(tv)
+                else:
+                    table += "{}".format(tv)
+            table += "|"
+
+        table += "\n"
+
+    return table
+
+
 def write_global_state_variable_table(state):
     out = ""
     out += "<h3>Global State</h3>"
@@ -57,5 +84,5 @@ def write_state_section(state, links=False):
     out += "\n"
     out += "### Variable Table"
     out += "\n"
-    out += write_state_variable_table(state, links=links)
+    out += write_state_variable_table_markdown(state, links=links)
     return out
