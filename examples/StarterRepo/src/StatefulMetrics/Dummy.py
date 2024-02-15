f3fb4ad78763177d7f1a1831f45e08c8033485ef
SeanMcOwen <seanmcowen@gmail.com>
2024-02-15 13:50:45 -0500

    Finish example repo revise and update all reports

--- examples/StarterRepo/src/StatefulMetrics/Dummy.py
+++ examples/StarterRepo/src/StatefulMetrics/Dummy.py
@@ -1,12 +1,18 @@
 from ..Types import DummyType2
 
 
-dummy_stateful_metric = {"name": "Dummy Stateful Metrics",
-                           "notes": None,
-                           "metrics": [{"type": DummyType2,
-                                        "name": "dummy_metric",
-                                        "description": "Dummy Dummy",
-                                        "variables_used": ["Variable A"],
-                                        "parameters_used": [],
-                                        "symbol": None,
-                                        "domain": None}]}
+dummy_stateful_metric = {
+    "name": "Dummy Stateful Metrics",
+    "notes": None,
+    "metrics": [
+        {
+            "type": DummyType2,
+            "name": "dummy_metric",
+            "description": "Dummy Dummy",
+            "variables_used": [("Dummy State", "Variable A")],
+            "parameters_used": [],
+            "symbol": None,
+            "domain": None,
+        }
+    ],
+}
