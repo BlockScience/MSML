eb131538deb08e1758c05132bd5f292345402de5
SeanMcOwen <seanmcowen@gmail.com>
2024-02-18 23:13:35 -0500

    Add convert schema prefix code

--- src/schema.py
+++ src/schema.py
@@ -3,4 +3,6 @@ import json
 
 current_path = pathlib.Path(__file__).parent.resolve()
 with open("{}/schema.schema.json".format(current_path), "r") as f:
-    schema = json.loads(f.read())
+    f = f.read()
+f = f.replace("./schema.schema.json/", "")
+schema = json.loads(f)
