ef26f6455e9578669ef0f61fd6e598d45c97445b
SeanMcOwen <seanmcowen@gmail.com>
2024-02-25 12:53:45 -0500

    Add metric json and validation

--- examples/StarterRepo/src/Metrics/Dummy.py
+++ examples/StarterRepo/src/Metrics/Dummy.py
@@ -6,11 +6,10 @@ metrics_x.append(
         "type": "Dummy Type 2",
         "name": "Metric A",
         "description": "Dummy metric",
-        "variables_used": [("Dummy State", "Variable A")],
+        "variables_used": [["Dummy State", "Variable A"]],
         "parameters_used": ["dummy_parameter2"],
         "domain": ["Dummy Space 1"],
         "logic": "",
         "symbol": None,
-        "domain": None,
     }
 )
