18279f2dbc0c389f08e12a11b03789a8a40007d1
SeanMcOwen <seanmcowen@gmail.com>
2024-03-11 12:48:25 -0400

    Start scaffolding type keys

--- src/Load/type.py
+++ src/Load/type.py
@@ -1,5 +1,6 @@
 from .general import check_json_keys
 from ..Classes import Type
+import os
 
 
 def convert_type(data, ms):
@@ -19,6 +20,20 @@ def convert_type(data, ms):
 
 def load_types(ms, json) -> None:
 
+    type_keys = load_type_keys()
+
     ms["Types"] = {}
     for data in json["Types"]:
         ms["Types"][data["name"]] = convert_type(data, ms)
+
+
+def load_python_type_key(path):
+    print("A")
+
+
+def load_type_keys() -> dict:
+    type_keys = {}
+    print(os.listdir("."))
+    python_path = "src/TypeMappings/types.py"
+    if os.path.exists(python_path):
+        type_keys["python"] = load_python_type_key(python_path)
