20dcc8e2f0f9e935904beb07e64c886465ca835a
SeanMcOwen <seanmcowen@gmail.com>
2024-03-31 15:38:27 -0400

    Finish update to example

---
+++ examples/StarterRepo/model/parameters.py
@@ -0,0 +1,22 @@
+from .types import DummyType1
+from typing import TypedDict
+
+SystemParameters = TypedDict('SystemParameters', {'dummy_parameter2': DummyType1})
+
+BehavioralParameters = TypedDict('BehavioralParameters', {'dummy_parameter': DummyType1})
+
+FunctionalParameters = TypedDict('FunctionalParameters', {})
+
+Parameters = TypedDict("Parameters",{**BehavioralParameters.__annotations__,
+ **FunctionalParameters.__annotations__,
+**SystemParameters.__annotations__})
+
+functional_parameters: FunctionalParameters = {}
+
+behavioral_parameters: BehavioralParameters = {"dummy_parameter": None,
+}
+
+system_parameters: SystemParameters = {"dummy_parameter2": None,
+}
+
+parameters: Parameters = {**behavioral_parameters, **functional_parameters, **system_parameters}
--- examples/StarterRepo/reports/markdown/Wiring/Dummy Control Block.md
+++ examples/StarterRepo/reports/markdown/Wiring/Dummy Control Block.md
@@ -31,8 +31,8 @@ Dummy Control Block
 
 ## All Blocks
 1. [[Dummy Control Action]]
-2. [[Dummy Mechanism]]
-3. [[Dummy Policy]]
+2. [[Dummy Policy]]
+3. [[Dummy Mechanism]]
 
 ## Constraints
 
@@ -42,9 +42,9 @@ Dummy Control Block
 1. [[Terminating Space]]
 
 ## All Spaces Used
-1. [[Dummy Space 2]]
-2. [[Dummy Space 1]]
-3. [[Terminating Space]]
+1. [[Terminating Space]]
+2. [[Dummy Space 2]]
+3. [[Dummy Space 1]]
 
 ## Parameters Used
 1. [[dummy_parameter]]
