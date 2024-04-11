05ab4089e47be1d8e41e857e447b7869b4360984
SeanMcOwen <seanmcowen@gmail.com>
2024-04-11 17:31:39 -0400

    Dev progress

--- dev/Type Test.jl
+++ dev/Type Test.jl
@@ -1,7 +1,10 @@
+include("../../cadCAD.jl/src/spaces.jl")
+include("../examples/StarterRepo/src/TypeMappings/types.jl")
+
+
+
 x::DummyType2 = 100
 y::DummyType1 = "100"
 z::DummyCompoundType = DummyCompoundType(y, x)
 
-print(x)
-print(y)
-print(z)
+print(Spaces.generate_space_type)
