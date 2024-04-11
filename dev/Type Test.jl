9ee700c537523ae0feee716d130f758c89c8ed32
SeanMcOwen <seanmcowen@gmail.com>
2024-04-11 17:40:28 -0400

    Created first space

--- dev/Type Test.jl
+++ dev/Type Test.jl
@@ -7,4 +7,5 @@ x::DummyType2 = 100
 y::DummyType1 = "100"
 z::DummyCompoundType = DummyCompoundType(y, x)
 
-print(Spaces.generate_space_type)
+Spaces.generate_space_type((a=DummyType1,b=DummyType2), "Test")
+print(Spaces.Test("A",100))
