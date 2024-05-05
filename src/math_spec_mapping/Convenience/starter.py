8a581e022d5c5871f6254f41ba50b11e4bb2d104
SeanMcOwen <seanmcowen@gmail.com>
2024-05-05 17:23:57 -0400

    Stateful metrics deletion

--- src/math_spec_mapping/Convenience/starter.py
+++ src/math_spec_mapping/Convenience/starter.py
@@ -2,4 +2,42 @@ import os
 
 
 def remove_dummy_repo_components(path):
-    print(os.listdir(path))
+
+    directory_folders = os.listdir(path)
+
+    if "StatefulMetrics" in directory_folders:
+        path2 = path + "/StatefulMetrics"
+        contents = os.listdir(path2)
+        if "Dummy.py" in contents:
+            os.remove(path2 + "/Dummy.py")
+        if "__init__.py" in contents:
+            with open(path2 + "/__init__.py", "r") as f:
+                contents = f.read()
+                contents = contents.replace(
+                    "from .Dummy import dummy_stateful_metrics\n", ""
+                )
+                contents = contents.replace(
+                    "stateful_metrics.extend(dummy_stateful_metrics)\n", ""
+                )
+            with open(path2 + "/__init__.py", "w") as f:
+                f.write(contents)
+
+    [
+        "StatefulMetrics",
+        "Metrics",
+        "Mechanisms",
+        "BoundaryActions",
+        "Types",
+        ".DS_Store",
+        "ControlActions",
+        "Displays",
+        "Spaces",
+        "__init__.py",
+        "__pycache__",
+        "State",
+        "Policies",
+        "Wiring",
+        "Parameters",
+        "TypeMappings",
+        "Entities",
+    ]
