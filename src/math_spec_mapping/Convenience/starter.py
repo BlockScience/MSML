ae42035865836143008dbba854392c9a7fe3cc03
SeanMcOwen <seanmcowen@gmail.com>
2024-05-05 22:18:47 -0400

    policy and mech remove

--- src/math_spec_mapping/Convenience/starter.py
+++ src/math_spec_mapping/Convenience/starter.py
@@ -67,6 +67,34 @@ def remove_dummy_repo_components(path):
             with open(path2 + "/__init__.py", "w") as f:
                 f.write(contents)
 
+    if "Mechanisms" in directory_folders:
+        path2 = path + "/Mechanisms"
+        contents = os.listdir(path2)
+        if "Dummy.py" in contents:
+            os.remove(path2 + "/Dummy.py")
+        if "__init__.py" in contents:
+            with open(path2 + "/__init__.py", "r") as f:
+                contents = f.read()
+                contents = contents.replace("from .Dummy import dummy_mechanisms\n", "")
+                contents = contents.replace("mechanism.extend(dummy_mechanisms)\n", "")
+                contents = contents.replace("mechanism.extend(dummy_mechanisms)", "")
+            with open(path2 + "/__init__.py", "w") as f:
+                f.write(contents)
+
+    if "Policies" in directory_folders:
+        path2 = path + "/Policies"
+        contents = os.listdir(path2)
+        if "Dummy.py" in contents:
+            os.remove(path2 + "/Dummy.py")
+        if "__init__.py" in contents:
+            with open(path2 + "/__init__.py", "r") as f:
+                contents = f.read()
+                contents = contents.replace("from .Dummy import dummy_policies\n", "")
+                contents = contents.replace("policies.extend(dummy_policies)\n", "")
+                contents = contents.replace("policies.extend(dummy_policies)", "")
+            with open(path2 + "/__init__.py", "w") as f:
+                f.write(contents)
+
     [
         "Mechanisms",
         "BoundaryActions",
@@ -76,7 +104,6 @@ def remove_dummy_repo_components(path):
         "__init__.py",
         "State",
         "Policies",
-        "Wiring",
         "Parameters",
         "TypeMappings",
         "Entities",
