e2b2e04907f487dbca50add2e833e8e6bc2d4172
SeanMcOwen <seanmcowen@gmail.com>
2024-06-02 23:40:25 -0400

    Update control action

--- examples/StarterRepo/src/ControlActions/Dummy.py
+++ examples/StarterRepo/src/ControlActions/Dummy.py
@@ -1,12 +1,13 @@
 dummy_control_action_v1_option = {
     "name": "V1 Dummy Control",
-    "description": "Description",
-    "logic": "A+B=C",
+    "description": "Equal weighted probability of choosing D, E or F each time",
+    "logic": "Select D, E, F with equal probabilities",
 }
+
 
 dummy_control_action = {
     "name": "Dummy Control Action",
-    "description": "Dummy",
+    "description": "Randomly returns any length 1 string of combinations of D, E, F",
     "constraints": [],
     "control_action_options": [dummy_control_action_v1_option],
     "codomain": [
