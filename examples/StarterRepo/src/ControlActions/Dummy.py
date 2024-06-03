5850fb67d21a4b94c5db0d6db3d93084793be16f
SeanMcOwen <seanmcowen@gmail.com>
2024-06-02 23:52:06 -0400

    Add control action

--- examples/StarterRepo/src/ControlActions/Dummy.py
+++ examples/StarterRepo/src/ControlActions/Dummy.py
@@ -4,12 +4,21 @@ dummy_control_action_v1_option = {
     "logic": "Select D, E, F with equal probabilities",
 }
 
+dummy_control_action_v2_option = {
+    "name": "V2 Dummy Control",
+    "description": "50% \chance of choosing D, 25% \chance of choosing E or 25% \of eachoosing F each time",
+    "logic": "Select D, E, F with those probabilities",
+}
+
 
 dummy_control_action = {
     "name": "Dummy Control Action",
     "description": "Randomly returns any length 1 string of combinations of D, E, F",
     "constraints": [],
-    "control_action_options": [dummy_control_action_v1_option],
+    "control_action_options": [
+        dummy_control_action_v1_option,
+        dummy_control_action_v2_option,
+    ],
     "codomain": [
         "Dummy Space 1",
     ],
--- examples/StarterRepo/src/Implementations/Python/ControlActions/Dummy.py
+++ examples/StarterRepo/src/Implementations/Python/ControlActions/Dummy.py
@@ -3,3 +3,7 @@ from random import choice
 
 def v1_dummy_control(state, params, spaces):
     return [{"a": choice(["D", "E", "F"])}]
+
+
+def v2_dummy_control(state, params, spaces):
+    return [{"a": choice(["D", "D", "E", "F"])}]
