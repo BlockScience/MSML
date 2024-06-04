c2157ee7924beb8d84175157cb5ef8a22a626229
SeanMcOwen <seanmcowen@gmail.com>
2024-06-04 15:39:32 -0400

    Julia playground

--- dev/Type Test.jl
+++ dev/Type Test.jl
@@ -9,4 +9,22 @@ z::DummyCompoundType = DummyCompoundType(y, x)
 
 generate_space_type((a=DummyType1,b=DummyType2), "Test")
 Test = Spaces.Test("A",100)
-print(Test)
+
+function atest()::NamedTuple{(:e, :f), Tuple{Spaces.Test, Spaces.Test}}
+    return (e = Spaces.Test(a="A", b=100), f = Spaces.Test(a="A", b=100))
+end
+
+
+
+
+
+println(Base.return_types(atest))
+
+# Get the methods of the function
+#m = methods(atest)
+
+# Print the signatures of the methods
+#for method in m
+#    println(method.sig)
+#end
+
