b986b090395e12805181ced84fc04f804e26153e
SeanMcOwen <seanmcowen@gmail.com>
2024-11-11 23:17:00 -0500

    Checkpoint

--- src/math_spec_mapping/Convenience/github.py
+++ src/math_spec_mapping/Convenience/github.py
@@ -2,4 +2,16 @@ import os
 
 
 def write_scaffold_to_github_issues(scaffolder_folder):
-    print(os.listdir(scaffolder_folder))
+    folders = [
+        x
+        for x in os.listdir(scaffolder_folder)
+        if os.path.isdir(scaffolder_folder + "/" + x)
+    ]
+    for folder in folders:
+        issue_name = "Write {} from MSML Scaffold".format(folder)
+        checklist = [
+            x[:-3]
+            for x in os.listdir(scaffolder_folder + "/" + folder)
+            if x.endswith(".md")
+        ]
+        print(checklist)
