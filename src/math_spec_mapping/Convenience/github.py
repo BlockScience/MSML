3d499412bdd32432f1960a1065879c6c50ff22f3
SeanMcOwen <seanmcowen@gmail.com>
2024-11-11 23:34:11 -0500

    Checkpoint

--- src/math_spec_mapping/Convenience/github.py
+++ src/math_spec_mapping/Convenience/github.py
@@ -1,7 +1,8 @@
 import os
+import requests
 
 
-def write_scaffold_to_github_issues(scaffolder_folder):
+def write_scaffold_to_github_issues(scaffolder_folder, repo_owner, repository_name):
     folders = [
         x
         for x in os.listdir(scaffolder_folder)
@@ -14,4 +15,14 @@ def write_scaffold_to_github_issues(scaffolder_folder):
             for x in os.listdir(scaffolder_folder + "/" + folder)
             if x.endswith(".md")
         ]
-        print(checklist)
+        checklist = ["- [ ] " + x for x in checklist]
+        checklist = "\n".join(checklist)
+
+        print(
+            requests.post(
+                "https://api.github.com/repos/{}/{}/issues".format(
+                    repo_owner, repository_name
+                ),
+                data={"title": issue_name, "body": checklist},
+            ).text
+        )
