8dbe7f94a9c525487e1aaceb4ac05ba03c454b1d
SeanMcOwen <seanmcowen@gmail.com>
2025-02-11 11:31:29 -0500

    convert_markdown_to_pdf

--- src/math_spec_mapping/Reports/__init__.py
+++ src/math_spec_mapping/Reports/__init__.py
@@ -2,7 +2,7 @@ from .node_map import create_action_chains_graph
 from .boundary_actions import write_out_boundary_actions
 from .policies import write_out_policies
 from .mechanisms import write_out_mechanisms
-from .general import load_svg_graphviz
+from .general import load_svg_graphviz, convert_markdown_to_pdf
 from .html import (
     write_basic_report_full,
     write_action_chain_reports,
